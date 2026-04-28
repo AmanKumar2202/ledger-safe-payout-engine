6d6baf4fdf9e12de23f31bc8a56a130b72966088
AmanKumar2202 <amankumar220203@gmail.com>
2026-04-28 21:47:50 +0530

    fix: expose balance_paise in merchant admin

--- backend/merchants/admin.py
+++ backend/merchants/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Merchant, LedgerEntry
 
-admin.site.register(Merchant)
-admin.site.register(LedgerEntry)
+@admin.register(Merchant)
+class MerchantAdmin(admin.ModelAdmin):
+    list_display = ['name', 'balance_paise']
+
+@admin.register(LedgerEntry)
+class LedgerEntryAdmin(admin.ModelAdmin):
+    list_display = ['merchant', 'amount_paise', 'entry_type', 'reference_id']
